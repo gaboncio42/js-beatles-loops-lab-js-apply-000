6461042c7f9551c6ef71bbaf5d87cfb1a046bf84
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-20 11:30:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,3 +35,12 @@ function johnLennonFacts(facts) {
   }
   return array;
 }
+
+function iLoveTheBeatles(number) {
+  var array = [];
+  do {
+    array.push("I love the Beatles!");
+    number++;
+  } while (number < 15);
+  return array;
+}
