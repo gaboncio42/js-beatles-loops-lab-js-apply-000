da4e7b829c6a20bcd474f8609e356029bf33c30e
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-20 11:21:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,10 @@ const musicians = [
     "Lead Guitar",
     "Drums"];
     
-    
+    function theBeatlesPlay(musicians, instruments) {
+      const array = [];
+      for(let i = 0; i < musicians.length; i++) {
+        array.push(musicians[i] + " plays " + instruments[i])
+      }
+      return array;
+    }
