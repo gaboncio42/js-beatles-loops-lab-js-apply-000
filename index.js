1d478bb1177c45dfde9606e80b00198201842dc0
Gabriel Seijo <gabrielseijo.m@gmail.com>
2018-08-20 11:18:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 // add solution here
-const musicians = ["John Lennon", "Paul McCartney",]
+const musicians = [
+  "John Lennon", 
+  "Paul McCartney", 
+  "George Harrison",
+  "Ringo Star"];
+  
+  const instruments = [
+    "Guitar",
+    "Bass Guitar",
+    "Lead Guitar",
+    "Drums"];
+    
+    
